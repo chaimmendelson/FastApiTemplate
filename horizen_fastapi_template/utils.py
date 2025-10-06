7ff9983080138a0d65003f4239cddeff5569be5e
chaim <chaimke2005@gmail.com>
2025-10-06 15:36:47 +0300

    feat move to horizon-fastapi-template

--- horizen_fastapi_template/utils.py
+++
@@ -1,4 +0,0 @@
-from ._internal.database import BitbucketAPI, AsyncFTPClient, BaseAPI, get_dynamic_client
-from ._internal.models import GraphQLVersion
-
-__all__ = ["BitbucketAPI", "AsyncFTPClient", "BaseAPI", "get_dynamic_client", "GraphQLVersion"]
--- horizon_fastapi_template/_internal/database/__init__.py
+++ horizon_fastapi_template/_internal/database/__init__.py
@@ -3,6 +3,5 @@
 from .basic_api import BaseAPI
 from .ftp_client import AsyncFTPClient
 from .kube_client import get_dynamic_client
-from .bitbucket_api import BitbucketAPI
 
 __all__ = ["BaseAPI", "AsyncFTPClient", "get_dynamic_client"]
