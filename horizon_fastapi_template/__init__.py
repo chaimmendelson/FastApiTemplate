e5b9ae8b5df21ba5b22b8b72a613f37f6019948e
Yehonatan Magen <yehonatanmagen@Yehonatans-MacBook-Air.local>
2025-10-07 20:55:04 +0300

    allow importing BaseAPI

--- horizon_fastapi_template/__init__.py
+++ horizon_fastapi_template/__init__.py
@@ -1,10 +1,13 @@
 """FastAPI application template package."""
 from ._internal import general_create_app
+from ._internal.database.basic_api import BaseAPI
 
-__all__ = ["general_create_app"]
+__all__ = ["general_create_app", "BaseAPI"]
 
 
 def __getattr__(name: str):
     if name == "general_create_app":
         return general_create_app
+    elif name == "BaseAPI":
+        return BaseAPI
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
