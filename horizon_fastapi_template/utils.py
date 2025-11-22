a76cc6905ff27ef9889c8b0b89a3c4d1794a415f
chaim <chaimke2005@gmail.com>
2025-11-23 00:31:53 +0200

    Fix: Added settings to utils.py

--- horizon_fastapi_template/utils.py
+++ horizon_fastapi_template/utils.py
@@ -1,4 +1,11 @@
 from ._internal.database import AsyncFTPClient, BaseAPI, get_dynamic_client
 from ._internal.models import GraphQLVersion
+from ._internal.utils import settings
 
-__all__ = ["AsyncFTPClient", "BaseAPI", "get_dynamic_client", "GraphQLVersion"]
+__all__ = [
+    "AsyncFTPClient",
+    "BaseAPI",
+    "get_dynamic_client",
+    "GraphQLVersion",
+    "settings"
+]
